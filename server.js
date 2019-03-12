7d94c8a7a7a10a490ed7eb65c775cc298acce0e1
Mohak666 <mohak.rathore06@gmail.com>
2019-03-12 19:07:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')// __dirname storepath to project directory
+app.set('view engine','hbs');
+
+// middleware --- app.use() is used to register middleware
+// *** middleware executes in order of definition ***
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.originalUrl}`
+  fs.appendFile('server.log',log + "\n", (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  console.log(log)
+  next();// next() runs when middleware function is done,
+     // if next() is not called , any of the routes(/*) will never fire
+});
+
+app.use(express.static(__dirname + '/public' ));
+
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+
+//********handlers*********
+
+
+app.get('/',(req,res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to home page',
+  });
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+      pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+      errorMessage:'unable to handle request'
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+//
