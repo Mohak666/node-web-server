bcdeead826f97feef6880d1361e079266d38107c
Mohak666 <mohak.rathore06@gmail.com>
2019-03-13 18:13:41 +0530

    added projects page

--- server.js
+++ server.js
@@ -62,6 +62,13 @@ app.get('/bad',(req,res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Projects',
+    message : 'Portfolio page here',
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
